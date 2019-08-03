a960e410eee0f99ad497008b326f708f274b5caf
Karter13 <KarMiH1975@mail.ru>
2019-08-03 17:18:52 +0300

    написал скрипт на автоматическое закрытие модального окна через 5с

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,29 @@
 let button = document.querySelector("#button");
 let modal = document.querySelector("#modal");
-let close = document.querySelector("#close");
+
+// получение кнопки ЗАКРЫТЬ
+// let close = document.querySelector("#close");
+
+
 
 button.addEventListener("click", function () {
   modal.classList.add("modal_activ");
+
+  window.setTimeout(function () {
+    modal.classList.remove("modal_activ");
+  }, 5000);
+
 });
 
-close.addEventListener("click", function () {
-  modal.classList.remove("modal_activ");
-});
+
+
+// function timer() {
+//   modal.classList.remove("modal_activ");
+// }
+
+//  убирает модальное окно при нажатии кнопки ЗАКРЫТЬ
+
+// close.addEventListener("click", function () {
+//   modal.classList.remove("modal_activ");
+// });
+
