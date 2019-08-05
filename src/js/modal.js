024640685e6d7a0d58706f8a40b6b8cad959f9e3
Karter13 <KarMiH1975@mail.ru>
2019-08-05 22:58:17 +0300

    Подключил jquery, закодил в HTML кнопку скрола вверх, создал файл _button.sass и подключил к style.sass, написал скрипт jquery для скрол кнопки

---
+++ src/js/modal.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    let button = $("#button");
+    let modal = $("#modal");
+    let close = $("#close");
+
+    button.on("click", function() {
+        modal.addClass("modal_activ");
+    });
+
+    close.on("click", function() {
+        modal.removeClass("modal_activ");
+    });
+});
+
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+
+        if ($(this).scrollTop() > 50) {
+            $('#button-up').fadeIn();
+        } else {
+            $('#button-up').fadeOut();
+        }
+    });
+
+    $('#button-up').click(function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+});
