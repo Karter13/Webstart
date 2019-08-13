d545cf2bc003ce1f06c659bda01584a97166d7f0
Karter13 <KarMiH1975@mail.ru>
2019-08-13 18:22:25 +0300

    написал HTML код для секции materials, создал файл _materials.sass и прописал в нем стили к секции materials, адаптировал данную секцию

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
-let button = document.querySelector("#button");
-let modal = document.querySelector("#modal");
+// let button = document.querySelector("#button");
+// let modal = document.querySelector("#modal");
 
 // получение кнопки ЗАКРЫТЬ
 // let close = document.querySelector("#close");
 
 
 
-button.addEventListener("click", function () {
-  modal.classList.add("modal_activ");
+// button.addEventListener("click", function() {
+//     modal.classList.add("modal_activ");
 
-  window.setTimeout(function () {
-    modal.classList.remove("modal_activ");
-  }, 5000);
+//     window.setTimeout(function() {
+//         modal.classList.remove("modal_activ");
+//     }, 5000);
 
-});
+// });
 
 
 
@@ -27,3 +27,20 @@ button.addEventListener("click", function () {
 //   modal.classList.remove("modal_activ");
 // });
 
+// подключение карты
+
+function initMap() {
+
+    let pos = { lat: 55.825847, lng: 37.318093 };
+    let opt = {
+        center: pos,
+        zoom: 15
+    };
+    let map = new google.maps.Map(document.getElementById('map'), opt);
+
+    let marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: " г. Москва, ул. Ленина, д. 10, корпус 2, оф. 308"
+    });
+}
